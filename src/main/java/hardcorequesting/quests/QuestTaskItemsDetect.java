3e8704730c67705f5af215d9273455341d3cf5f3
knoxz <promolitor@gmail.com>
2015-11-11 23:21:23 +0100

    Update QuestTaskItemsDetect.java
    Add a Timerout for the countItems Method, to avoid Servertimeouts with large Itemstacks. For example Botania Itemduplication.

--- src/main/java/hardcorequesting/quests/QuestTaskItemsDetect.java
+++ src/main/java/hardcorequesting/quests/QuestTaskItemsDetect.java
@@ -13,6 +13,12 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 
 public class QuestTaskItemsDetect extends QuestTaskItems {
+    
+    private long time;
+	HashMap<String, Integer> playerCount = new HashMap<String, Integer>();
+	HashMap<String, Long> playerTime = new HashMap<String, Long>();
+	HashMap<String, Boolean> playerTimeout = new HashMap<String, Boolean>();
+	
     public QuestTaskItemsDetect(Quest parent, String description, String longDescription) {
         super(parent, description, longDescription);
 
@@ -36,12 +42,35 @@ public class QuestTaskItemsDetect extends QuestTaskItems {
     public void onUpdate(EntityPlayer player, DataReader dr) {
         countItems(player, null);
     }
+    
     @Override
-    public void onItemPickUp(EntityItemPickupEvent event) {
-        if(event.entityPlayer.inventory.inventoryChanged) {
-            countItems(event.entityPlayer, event.item.getEntityItem());
-        }
-    }
+	public void onItemPickUp(EntityItemPickupEvent event) {
+		time = System.currentTimeMillis();
+		String name = event.entityPlayer.getDisplayName();
+
+		// First time events triggers for player. Fill list to avoid null
+		if (!playerTime.containsKey(name)) {
+			playerTime.put(name, time);
+			playerCount.put(name, 1);
+			playerTimeout.put(name, false);
+		}
+		// Time in Millisecond to timeout. When outside this timeframe reset
+		// counting to 1
+		if ((time - playerTime.get(name)) > 3000) {
+			playerCount.put(name, 1);
+			playerTime.put(name, time);
+			playerTimeout.put(name, false);
+		} else if(!playerTimeout.get(name)){ // When inside the timeframe add 1 to the counter. If already there is already a timeout. No need to check again.
+			playerCount.put(name, playerCount.get(name) + 1);
+			if (playerCount.get(name) > 30) { // if counter reaches 30. add timeout for the rest of the duration.
+				playerTimeout.put(name, true);
+			}
+		}
+
+		if (!playerTimeout.get(name) && event.entityPlayer.inventory.inventoryChanged) {
+			countItems(event.entityPlayer, event.item.getEntityItem());
+		}
+	}
 
     @Override
     public void onCrafting(PlayerEvent.ItemCraftedEvent event) {
